e474352c857fc2d735f64b9e6b0c4fa6760af333
Mayank aneja <anejamayank16@gmail.com>
2025-10-17 19:03:29 +0530

    shadcn init

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,20 +1,10 @@
 "use client"
-import { useSession, signIn, signOut } from "next-auth/react"
+import React, { useState } from "react"
 
-export default function Component() {
-  const { data: session } = useSession()
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
-  }
-  return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
-  )
+const page = ()=>{
+  return <div>
+    Page
+  </div>
 }
+
+export default page
